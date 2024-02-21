229265349976589e45743e42f739b063fcdd00bc
WizVader <126302304+WizVader@users.noreply.github.com>
2024-02-21 22:19:46 +1300

    Websraped a 100 best movies website and added it to a .txt file

--- bs4-start/Starting Code - 100 movies to watch start/main.py
+++ bs4-start/Starting Code - 100 movies to watch start/main.py
@@ -1,8 +1,17 @@
 import requests
 from bs4 import BeautifulSoup
 
-URL = "https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/"
+URL = "https://www.empireonline.com/movies/features/best-movies-2/"
 
 # Write your code below this line 👇
 
+response = requests.get(URL)
+webpage = response.text
+soup = BeautifulSoup(webpage, "html.parser")
+movie_tags = soup.find_all(name="h3", class_="listicleItem_listicle-item__title__BfenH")
+movies_text = [(movie_tag.text + '\n') for movie_tag in movie_tags]
+movies_text.reverse()
+
+with open("movies.txt", 'w') as file:
+    file.writelines(movies_text)
 
