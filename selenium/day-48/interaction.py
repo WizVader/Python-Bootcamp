6c7be0c4c8e86be5646c08a6461dbc47d13ea274
WizVader <126302304+WizVader@users.noreply.github.com>
2024-02-28 15:05:22 +1300

    Practiced sending keys and clicking buttons through selenium

--- selenium/day-48/interaction.py
+++ selenium/day-48/interaction.py
@@ -1,16 +1,25 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("detach", True)
 
-LINK = "https://en.wikipedia.org/wiki/Main_Page"
+LINK = "https://secure-retreat-92358.herokuapp.com/"
 
 driver = webdriver.Chrome(options=chrome_options)
 driver.get(LINK)
 
-div = driver.find_element(By.ID, value="articlecount")
-number_of_articles = div.find_element(By.TAG_NAME, "a").text
-print(number_of_articles)
+first_name = driver.find_element(By.NAME, value="fName")
+first_name.send_keys("Charanjeet")
+
+last_name = driver.find_element(By.NAME, value="lName")
+last_name.send_keys("Viswanathan")
+
+email = driver.find_element(By.NAME, value="email")
+email.send_keys("svcharanjeet@gmail.com")
+
+sign_up_button = driver.find_element(By.TAG_NAME, value="button")
+sign_up_button.click()
 
 driver.quit()
