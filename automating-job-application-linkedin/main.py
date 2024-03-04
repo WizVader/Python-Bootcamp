7206c16c7da07a8060e310b9ceba282bf201bcca
WizVader <126302304+WizVader@users.noreply.github.com>
2024-03-05 11:51:24 +1300

    Unable to find correct element. Will work on it later

--- automating-job-application-linkedin/main.py
+++ automating-job-application-linkedin/main.py
@@ -3,9 +3,7 @@ from selenium.webdriver.common.by import By
 import os
 import time
 
-URL = ("https://www.linkedin.com/jobs/search/?currentJobId=3823155509&f_AL=true&geoId=102257491&keywords=python"
-       "%20developer&location=London%2C%20England%2C%20United%20Kingdom&origin=JOB_SEARCH_PAGE_LOCATION_AUTOCOMPLETE"
-       "&refresh=true")
+URL = ("https://www.linkedin.com/jobs/search/?currentJobId=3823155509&f_AL=true&geoId=102257491&keywords=python%20developer&location=London%2C%20England%2C%20United%20Kingdom&origin=JOB_SEARCH_PAGE_SEARCH_BUTTON&refresh=true")
 
 LINKEDIN_EMAIL = os.environ['LINKEDIN_EMAIL']
 LINKEDIN_PASSWORD = os.environ['LINKEDIN_PASSWORD']
@@ -17,9 +15,6 @@ driver = webdriver.Chrome(options=chrome_options)
 driver.get(URL)
 
 time.sleep(2)
-
-# auth_wall_sign_in_button = driver.find_element(By.CSS_SELECTOR, value=".join-form .authwall-join-form__form-toggle--bottom")
-# auth_wall_sign_in_button.click()
 
 sign_in_button_tag = driver.find_element(By.CLASS_NAME, value="nav__button-secondary")
 print(sign_in_button_tag.text)
@@ -34,3 +29,8 @@ password_input_tag.send_keys(LINKEDIN_PASSWORD)
 sign_in_button_tag_login_page = driver.find_element(By.CLASS_NAME, value="btn__primary--large")
 sign_in_button_tag_login_page.click()
 
+job_listings = driver.find_elements(by=By.CSS_SELECTOR, value=".job-card-container--clickable")
+
+for listing in job_listings:
+    listing.click()
+    time.sleep(2)
