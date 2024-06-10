75917e92fa2792eb3babdbe5b0a3a2155b748d4b
WizVader <126302304+WizVader@users.noreply.github.com>
2024-06-10 18:03:36 +1200

    Updated automation script to work with X migration

--- twitter-internet-complaint-bot/twitter_internet_complaint_bot.py
+++ twitter-internet-complaint-bot/twitter_internet_complaint_bot.py
@@ -27,7 +27,7 @@ class InternetSpeedTwitterBot:
         go_button.click()
         time.sleep(60)
         close_button = self.driver.find_element(By.XPATH,
-                                                value='//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div[3]/div/div[8]/div/div/p[2]/button')
+                                                value='//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div[3]/div/div[8]/div/div/div[2]/a')
         close_button.click()
         self.calculated_download = (self.driver.find_element(By.XPATH,
                                                              value='//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div[3]/div/div[3]/div/div/div[2]/div[1]/div[1]/div/div[2]/span')).text
@@ -42,23 +42,23 @@ class InternetSpeedTwitterBot:
         self.driver.get("https://twitter.com/i/flow/login")
         time.sleep(5)
         email_input = self.driver.find_element(By.XPATH,
-                                               value='//*[@id="layers"]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div/div/div/div[5]/label/div/div[2]/div/input')
+                                               value='//*[@id="layers"]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div/div/div/div[4]/label/div/div[2]/div/input')
         email_input.send_keys(TWITTER_USERNAME)
         time.sleep(5)
         next_button = self.driver.find_element(By.XPATH,
-                                               value='//*[@id="layers"]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div/div/div/div[6]/div')
+                                               value='//*[@id="layers"]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div/div/div/button[2]')
         next_button.click()
         time.sleep(5)
         password_input_div = self.driver.find_element(By.XPATH,
-                                                  value='//*[@id="layers"]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[1]/div/div/div[3]/div/label/div/div[2]')
+                                                  value='//*[@id="layers"]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[1]/div/div/div[3]/div/label/div/div[2]/div[1]')
         password_input_box = password_input_div.find_element(By.TAG_NAME, value="input")
         password_input_box.send_keys(TWITTER_PASSWORD)
         time.sleep(5)
         login_button = self.driver.find_element(By.XPATH,
-                                                value='//*[@id="layers"]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[2]/div/div[1]/div/div/div/div')
+                                                value='//*[@id="layers"]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[2]/div/div[1]/div/div/button')
         login_button.click()
         time.sleep(5)
-        tweet_input_div = self.driver.find_element(By.XPATH, value='//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div/div/div[3]/div/div[2]/div[1]/div/div/div/div[2]/div[1]/div/div/div/div/div/div/div/div/div/div/label/div[1]/div/div/div/div/div/div[2]/div/div/div/div')
+        tweet_input_div = self.driver.find_element(By.XPATH, value='//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div[1]/div/div[3]/div/div[2]/div[1]/div/div/div/div[2]/div[1]/div/div/div/div/div/div/div/div/div/div/div/div[1]/div/div/div/div/div/div[2]/div/div/div/div')
         tweet_input_div.send_keys(f"This is an automated tweet as part of a project. The internet download speed is {self.calculated_download}, upload speed is {self.calculated_download}")
-        post_button = self.driver.find_element(By.XPATH, value='//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div/div/div[3]/div/div[2]/div[1]/div/div/div/div[2]/div[2]/div[2]/div/div/div/div[3]')
+        post_button = self.driver.find_element(By.XPATH, value='//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div[1]/div/div[3]/div/div[2]/div[1]/div/div/div/div[2]/div[2]/div[2]/div/div/div/button')
         post_button.click()
